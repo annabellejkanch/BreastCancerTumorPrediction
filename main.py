606a5d648e45cc610dc5cd649e59ab229d8ebde7
Annabelle Kanchirathingal <annabelle.kanch@gmail.com>
2024-11-09 23:27:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, jsonify
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
