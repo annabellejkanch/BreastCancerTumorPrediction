850af3bccc6d4dc064976a760ced46395d7abab2
Annabelle Kanchirathingal <annabelle.kanch@gmail.com>
2024-11-09 23:35:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ def predict():
         return render_template('index.html', features=features, error=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
