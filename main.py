1f1f69bb10cdb99f6fc8ced09c2d937c7a108d18
Annabelle Kanchirathingal <annabelle.kanch@gmail.com>
2024-11-14 03:01:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ scaler = joblib.load('scaler.pkl')
 
 def ValuePredictor(to_predict_list):
     # Convert the input list to a numpy array and reshape it to (1, 30)
-    to_predict = np.array(to_predict_list.values).reshape(1, 30)
+    to_predict = np.array(to_predict_list).reshape(1, 30)
     
     # Scale the input data
     scaled_data = scaler.transform(to_predict)
     
     # Make prediction using the model
-    result = model.predict(to_predict)
+    result = model.predict(scaled_data.values)
     
     return result[0][0]
 
